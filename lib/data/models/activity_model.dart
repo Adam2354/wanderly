7f1797635aa26962d5ca745c70c76555534f98bf
Adam2354 <khoiru.22135@mhs.unesa.ac.id>
2026-02-18 14:02:20 +0700

    Wanderly_mission_6

--- lib/data/models/activity_model.dart
+++ lib/data/models/activity_model.dart
@@ -1,5 +1,4 @@
 import 'package:hive/hive.dart';
-
 part 'activity_model.g.dart';
 
 @HiveType(typeId: 0)
--- lib/data/models/activity_model.g.dart
+++ lib/data/models/activity_model.g.dart
@@ -1,10 +1,4 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
 part of 'activity_model.dart';
-
-// **************************************************************************
-// TypeAdapterGenerator
-// **************************************************************************
 
 class ActivityModelAdapter extends TypeAdapter<ActivityModel> {
   @override
